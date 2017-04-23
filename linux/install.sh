24c0c25cd429012806f86e9c51a304675dffda72
Unknown <unknown@example.com>
2017-04-23 23:12:46 +0200

    added linux installation
    added linux install script with desktop entry
    changed config dir to home/.conWhatsapp
    fixed some bugs

---
+++ linux/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Install Whatsapp
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+mkdir -p /usr/share/conWhatsapp
+
+cp  -R $DIR/../conWhatsapp-linux-x64/* /usr/share/conWhatsapp/
+
+# cp to bin
+cp $DIR/whatsapp /usr/bin/
+chmod +x /usr/bin/whatsapp
+
+# cp desktop file
+cp $DIR/conWhatsapp.desktop /usr/share/applications/conWhatsapp.desktop
