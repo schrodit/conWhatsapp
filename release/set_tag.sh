07907b4ceff77499002cca3e7812639d15dbf127
schrodit <tschrodi96@googlemail.com>
2017-04-25 18:33:31 +0200

    change tag setting

--- release/set_tag.sh
+++ release/set_tag.sh
@@ -3,12 +3,12 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
 
-# bump package.json
-gulp bump
 VERSION = cat package.json | jq '.version'
 
+echo 'version: '
+echo $VERSION
+
 # Add tag and push to master.
-git commit -a -m "Bump and publish version $VERSION"
 git tag -a $VERSION -m "Travis build $VERSION pushed a tag."
 git push origin --tags
 git fetch origin
